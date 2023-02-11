ed809fefe67bb791c9512cf4e2684ef01ea179ce
akhil <86508192+akhildgithub@users.noreply.github.com>
2023-02-11 12:23:17 +0530

    Create script.js

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ setInterval(function() {
   document.getElementById('Sec').innerHTML = String(se).padStart(2, '0')
   document.getElementById('day').innerHTML = Day[day]
   
-
-  document.getElementById('Date').innerHTML = da + (da % 10 == 1 ? 'st ' : (da % 10 == 2 ? 'nd ' : (da % 10 == 3 ? 'rd ' : 'th ')))
-  document.getElementById('Month').innerHTML = month[mo] + ', '
+ document.getElementById('Date').innerHTML = da + (da == 11 ? 'th' : (da % 10 == 1 ? 'st ' : (da % 10 == 2 ? 'nd ' : (da % 10 == 3 ? 'rd ' : 'th '))));
+ document.getElementById('Month').innerHTML = month[mo] + ', '
   document.getElementById('Year').innerHTML = ye
 }, 1000)
